4bc30240d95f0c75dae0c31cddb0ac8996991206
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-18 23:26:03 +0600

    If and else implement

---
+++ Conditions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Conditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+
+        // if (age > 18) {
+        // System.out.println("You will get NId card");
+        // }
+
+        // else {
+        // System.out.println("You will not get nid card");
+        // }
+
+        // Check even and odd numbers
+        int x = sc.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("Even Number");
+        }
+
+        else {
+            System.out.println("Odd NUmber");
+        }
+    }
+}
