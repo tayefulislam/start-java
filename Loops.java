c589a2034871d16e9ea0e1152798d37044b4a876
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-21 00:58:54 +0600

    for ,while and do while loop done

--- Loops.java
+++ Loops.java
@@ -1,8 +1,29 @@
 public class Loops {
     public static void main(String[] args) {
-        for (int counter = 1; counter <= 10; counter++) {
-            System.out.print("Counter Number : ");
-            System.err.println(counter);
+        /* For Loop */
+        // for (int counter = 1; counter <= 10; counter++) {
+        // System.out.print("Counter Number : ");
+        // System.err.println(counter);
+        // }
+
+        /* while loop */
+
+        int i = 12;
+
+        while (i < 10) {
+            System.out.print(i);
+            i++;
+
         }
+
+        /* Do While */
+
+        // int i = 12;
+
+        do {
+            System.out.println("Hello");
+            i++;
+        } while (i < 10);
+
     }
 }
