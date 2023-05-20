b3a49afbd1f94a712fea425858ad9c3a0a9c59ac
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-20 08:51:29 +0600

    for loops added

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.print("Counter Number : ");
+            System.err.println(counter);
+        }
+    }
+}
