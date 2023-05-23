45d429f6e5a223e55a74235cd6b9f750dec00507
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-23 19:09:00 +0600

    Half , Inverted Half and Interted Half 180 Roatated Pyramid Created

--- Patterns.java
+++ Patterns.java
@@ -3,8 +3,8 @@ public class Patterns {
 
         // Solid Patterns Problem
 
-        int n = 4;
-        int m = 3;
+        // int n = 4;
+        // int m = 5;
 
         // for (int i = 1; i <= n; i++) {
 
@@ -17,17 +17,63 @@ public class Patterns {
 
         // Hollow Patterns
 
+        // for (int i = 1; i <= n; i++) {
+
+        // for (int j = 1; j <= m; j++) {
+
+        // if (i == 1 || j == 1 || i == n || j == m) {
+        // System.out.print("*");
+        // }
+
+        // else {
+        // System.out.print(" ");
+        // }
+
+        // }
+
+        // System.out.println();
+        // }
+
+        // /* Half Pyramid */
+
+        // int n = 4;
+
+        // for (int i = 1; i <= n; i++) {
+
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+
+        // }
+
+        /* Inverted Half Pyramid */
+
+        // int n = 4;
+
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        /* Inverted half pyramid - Rotated By 180 */
+
+        int n = 7;
+        // Outer Loop
+
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= m; j++) {
-                if (i == 1 || j == 1 || i == n || j == m) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
+
         }
 
     }
