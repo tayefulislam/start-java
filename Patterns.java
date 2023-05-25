368c14f69c8ef04ebd3aa93b0803549602b386f4
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-26 00:59:00 +0600

    0 1 tRIANGLE DONE

--- Patterns.java
+++ Patterns.java
@@ -104,19 +104,40 @@ public class Patterns {
 
         // Floyd's Triangle
 
-        int n = 5;
+        // int n = 5;
 
-        int number = 1;
+        // int number = 1;
+
+        // for (int i = 1; i <= n; i++) {
+
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(number + " ");
+        // number++;
+        // }
+        // System.out.println();
+        // }
+
+        /*
+         * 0 1 Triangle
+         * 
+         * 
+         */
+
+        int n = 5;
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
-                System.out.print(number + " ");
-                number++;
+                int sum = i + j;
+                if (sum % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
             }
             System.out.println();
-        }
 
+        }
     }
 
 }
