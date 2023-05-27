80455f7ab84657ca2c37ccde57384e5a69fc262c
tayefulislam <kazitayefulislam12@gmail.com>
2023-05-27 23:50:51 +0600

    note pore dekhote hobe

--- Patterns.java
+++ Patterns.java
@@ -123,20 +123,34 @@ public class Patterns {
          * 
          */
 
+        // int n = 5;
+
+        // for (int i = 1; i <= n; i++) {
+
+        // for (int j = 1; j <= i; j++) {
+        // int sum = i + j;
+        // if (sum % 2 == 0) {
+        // System.out.print(1 + " ");
+        // } else {
+        // System.out.print(0 + " ");
+        // }
+        // }
+        // System.out.println();
+
+        // }
+
         int n = 5;
+        // Print a solid rhombus.
 
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= i; j++) {
-                int sum = i + j;
-                if (sum % 2 == 0) {
-                    System.out.print(1 + " ");
-                } else {
-                    System.out.print(0 + " ");
-                }
+            for (int j = 1; j <= n - 1; j++) {
+                System.out.print(".");
             }
-            System.out.println();
-
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();// for line break
         }
     }
 
